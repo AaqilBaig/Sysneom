a97c8dbb8d53edf5aef008e6bcf126b0106c91c5
Unknown Sentinel <63234449+Unknownsentinel193@users.noreply.github.com>
2025-09-08 12:27:22 -0500

    Update index.tsx
    Updated to with relevant content

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -12,10 +12,10 @@ const Contact = () => {
               "
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Have an Awesome Idea in mind? 
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
-                Our support team will get back to you ASAP via email.
+                Lets discuss it and make your Ideas come true!.
               </p>
               <form>
                 <div className="-mx-4 flex flex-wrap">
